e72780f42ab1b84952e6258683d93b82d8180535
benaiah-ke <benaiahpraise@gmail.com>
2022-08-17 17:04:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 15.5;
+let num2 = 4;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = parseInt((Math.random()*10), 10);
+
+console.log(random);
+
+let num3 = 44;
+
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max (19, 17, 20);
